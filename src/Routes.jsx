360e0384265b399ac2f992465e8ef91a0ca8a4e3
Yegor Kirsanov <kirsanov.yegor@gmail.com>
2019-08-16 23:48:23 +0300

    delete icons

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -6,7 +6,6 @@ import Dashboard from "./views/Dashboard";
 import ProjectList from "./views/ProjectList";
 import UserList from "./views/UserList";
 import Typography from "./views/Typography";
-import Icons from "./views/Icons";
 import Account from "./views/Account";
 import Settings from "./views/Settings";
 import SignUp from "./views/SignUp";
@@ -24,7 +23,6 @@ export default class Routes extends Component {
         <Route component={UserList} exact path="/users" />
         <Route component={ProjectList} exact path="/projects" />
         <Route component={Typography} exact path="/typography" />
-        <Route component={Icons} exact path="/icons" />
         <ProtectRoute component={Account} exact path="/account" />
         <Route component={Settings} exact path="/settings" />
         <Route component={SignUp} exact path="/sign-up" />
